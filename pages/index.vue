557c9dc718ed24e7530fb45c8f71512b80d3d8bd
rizkidarmawan21 <rizkidarmawan.0402102@gmail.com>
2025-10-14 19:17:17 +0700

    feat: video carousel

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <ContentContainer>
+  <ContentContainer class="px-4 md:px-10 xl:px-20">
     <div class="py-10 flex flex-col xl:flex-row justify-between gap-5 lg:divide-x divide-grayscale-10">
       <section class="w-full xl:w-[720px] xl:shrink-0">
         <HomeHotNews />
@@ -9,8 +9,8 @@
         <WidgetLatestNews />
       </section>
     </div>
-    <div>main</div>
   </ContentContainer>
+  <HomeVideoCarousel />
 </template>
 
 <script lang="ts" setup></script>
